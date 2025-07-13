e9857a4a726d60b1f9fee7800b7e4b122df3dc4c
monnyrodrigo <monny.rg@gmail.com>
2025-07-13 14:09:46 -0600

    Add files via upload

---
+++ funciones.R
@@ -0,0 +1,18 @@
+#' Suma dos números
+#'
+#' Esta función suma dos valores numéricos simples.
+#'
+#' @param x Primer número.
+#' @param y Segundo número.
+#' @return La suma de x + y.
+#' @examples
+#' suma.dos.numeros(2, 5)
+#' @export
+suma.dos.numeros <- function(x, y) {
+  x + y
+}
+#' Retorna un texto fijo
+#' @export
+saludo <- function() {
+  "Hola desde mi paquete."
+}
